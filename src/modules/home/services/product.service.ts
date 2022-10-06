22dae54b4d500229bb5ad99466a156f1278ef394
Matheus Paixão <mpgs@cin.ufpe.br>
2022-10-05 22:12:38 -0300

    changed fetch requests for axios requests

--- src/modules/home/services/product.service.ts
+++ src/modules/home/services/product.service.ts
@@ -1,12 +1,16 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://semgluserver.cin.ufpe.br"
+});
+
 const url = "https://semgluserver.cin.ufpe.br";
 export async function getProduct(productId: number) {
     try {
-        const response = await fetch(url + "/product/" + productId.toString() + "/");
-        const data = await response.json();
-        const safetyData = await fetch(url + "/safety/" + data.idSafety.toString() + "/");
-        const safetyDataJson = await safetyData.json();
-        data["safetyCategory"] = safetyDataJson.description;
-        return data;
+        const axiosResponse = await instance.get("/product/" + productId.toString() + "/");
+        const axiosSafetyData = await instance.get("/safety/" + axiosResponse.data.idSafety.toString() + "/");
+        axiosResponse.data["safetyCategory"] = axiosSafetyData.data.description;
+        return axiosResponse.data;
     } catch (error) {
         console.log("não achei a database " + error);
         return null;
@@ -15,10 +19,15 @@ export async function getProduct(productId: number) {
 
 export async function getProductsByCategory(productCategory: string) {
     const productCategoryArray = productCategory.split(" ");
+    var params = new URLSearchParams();
+    params.append("search", productCategoryArray[0]);
+    params.append("product", "productCategory");
     try {
-        const response = await fetch(url + "/products/?search=" + productCategoryArray[0] + "&product=productCategory");
-        const data = await response.json();
-        return data;
+        const axiosResponse = await instance.get("/products/", {
+            params: params
+        });
+        // url do get = baseURL + "/products/?search=" + productCategoryArray[0] + "&product=productCategory"
+        return axiosResponse.data;
     } catch (error) {
         console.log("não achei a database " + error);
         return null;
@@ -35,10 +44,15 @@ export async function getProductsByName(productName: string) {
         finalStringToSearch = finalStringToSearch + "+" + item;
     }
     });
+    var params = new URLSearchParams();
+    params.append("search", finalStringToSearch);
+    params.append("product", "productName");
     try {
-        const response = await fetch(url + "/products/?search=" + finalStringToSearch + "&product=productName");
-        const data = await response.json();
-        return data;
+        const axiosResponse = await instance.get("/products/", {
+            params: params
+        });
+        // url do get = baseURL + "/products/?search=" + finalStringToSearch + "&product=productName"
+        return axiosResponse.data;
     } catch (error) {
         console.log("não achei a database " + error);
         return null;
