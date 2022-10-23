aba77cd544268e9fb4e5f584e216a26231592d88
addaesg <casg@cin.ufpe.br>
2022-10-23 19:11:14 -0300

    NavBar Style

--- src/shared/components/TabBar/tabBar.tsx
+++ src/shared/components/TabBar/tabBar.tsx
@@ -4,18 +4,21 @@ import { StyleSheet } from 'react-native';
 import { ProfilePage } from '../../../modules/profile/pages/ProfilePage/profilePage';
 import { HomePage } from '../../../modules/home/pages/HomePage/homePage';
 import Scanner from '../../../modules/barCodeScanner/components/scanner/scanner';
+import { scale, verticalScale } from "../../styles/scaling_units";
 
 const Tab = createBottomTabNavigator();
 
 export function TabBar() {
   return (
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator screenOptions={{headerShown:false, tabBarStyle:styles.navBar}}
+       tabBarOptions={{showLabel: false }}
+       tabBarIcon={{size:400}}> 
         <Tab.Screen 
             name="Home" 
-            component={HomePage} 
+            component={HomePage}
             options={{
                 tabBarLabel: 'Home',
-                tabBarIcon: ({ color, size }) => (<Ionicons name="home" color={color} size={size} />),}}
+                tabBarIcon: ({ color, size }) => (<Ionicons name="home" color={color} size={size*1.25} />)}}
         />
         <Tab.Screen 
             name="Scanner" 
@@ -23,15 +26,16 @@ export function TabBar() {
             options={{
                 tabBarLabel: 'Scanner',
                 unmountOnBlur: true,
-                tabBarIcon: ({ color, size }) => (<Ionicons name="barcode" color={color} size={size} />),}}
+      
+                tabBarIcon: ({ color, size }) => (<Ionicons name="scan-outline" color={color} size={size*1.25} />),}}
             />
-        <Tab.Screen 
+{/*         <Tab.Screen 
             name="Profile" 
             component={ProfilePage} 
             options={{
                 tabBarLabel: 'User',
-                tabBarIcon: ({ color, size }) => (<Ionicons name="person" color={color} size={size} />),}}
-            />
+                tabBarIcon: ({ color, size }) => (<Ionicons name="person" color={color} size={size*1.25} />),}}
+            /> */}
       </Tab.Navigator>
   );
 }
@@ -39,8 +43,12 @@ export function TabBar() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navBar:{
+    height: verticalScale(50)
+  }
+
 });
