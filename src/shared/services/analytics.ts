d33b6b6862484f32305fb5afd306b45b16733ac0
Arthur Henrique <39110264+thurcst@users.noreply.github.com>
2022-11-13 12:27:29 -0300

    feature: analytics class readY

---
+++ src/shared/services/analytics.ts
@@ -0,0 +1,27 @@
+import { Axios } from "axios";
+import { randomUUID } from "crypto";
+
+const axios = new Axios();
+
+export class Events {
+  private pseudo_id: string;
+
+  constructor() {
+    this.pseudo_id = randomUUID();
+  }
+
+  identify_user() {
+    return this.pseudo_id;
+  }
+
+  sendEvent(eventName: string) {
+    const body = {
+      name: eventName,
+      user_id: this.pseudo_id,
+    };
+
+    axios.post("https://y2gyrx.deta.dev/send", body).then((res) => {
+      console.log(res);
+    });
+  }
+}
